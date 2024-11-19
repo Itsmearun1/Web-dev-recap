99f53c698d4d8e5f1fb68024be62fc833232a78c
itsmearun1 <arundevvb1@gmail.com>
2024-11-19 16:21:03 +0530

    drum-project-adv js

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,49 @@
+let buttons = document.querySelectorAll(".drum");
+for (let i = 0; i < buttons.length; i++) {
+  var audio = new Audio("sounds/tom-1.mp3");
+  function checker(n) {
+    switch (n) {
+      case "w":
+        new Audio("sounds/tom-1.mp3").play();
+        break;
+      case "a":
+        new Audio("sounds/tom-2.mp3").play();
+        break;
+      case "s":
+        new Audio("sounds/tom-3.mp3").play();
+        break;
+      case "d":
+        new Audio("sounds/tom-4.mp3").play();
+        break;
+      case "j":
+        new Audio("sounds/snare.mp3").play();
+        break;
+      case "k":
+        new Audio("sounds/crash.mp3").play();
+        break;
+      case "l":
+        new Audio("sounds/kick-bass.mp3").play();
+        break;
+
+      default:
+        break;
+    }
+  }
+  function bAnimation(btn){
+    document.querySelector("."+btn).classList.add("pressed");
+    setTimeout(() => {
+        document.querySelector("."+btn).classList.remove("pressed");
+    }, 100);
+  }
+  buttons[i].addEventListener("click", function () {
+    var bVal = this.innerHTML;
+    checker(bVal);
+    bAnimation(bVal);
+  });
+
+  document.addEventListener("keydown", function (e) {
+    var val=e.key;
+    checker(val);
+    bAnimation(val)
+  });
+}
