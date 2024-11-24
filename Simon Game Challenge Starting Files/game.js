2443fada4fbbb471603b2a08de34ccfc6ddf155f
itsmearun1 <arundevvb1@gmail.com>
2024-11-24 23:40:11 +0530

    simon game-update

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,50 +1,65 @@
 var gamePattern = [];
 var userClickedPattern = [];
 var buttonColors = ["red", "blue", "green", "yellow"];
-var level=0;
-var started=false;
+var level = 0;
+var started = false;
 function playSound(url) {
   let audio = new Audio(url);
   audio.play();
 }
 
-function animatePress(btn){
-    btn.addClass("pressed");
-    setTimeout(() => {
-        btn.removeClass("pressed")
-    }, 100);
+function animatePress(btn) {
+  btn.addClass("pressed");
+  setTimeout(() => {
+    btn.removeClass("pressed");
+  }, 100);
 }
-
 
 function nextSequnece() {
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColors[randomNumber];
   gamePattern.push(randomChosenColour);
+  console.log(gamePattern, userClickedPattern);
   var newId = "#" + randomChosenColour;
   $(newId).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
   playSound("sounds/" + randomChosenColour + ".mp3");
   level++;
-  $("h1").text("level "+level);
-
+  $("h1").text("level " + level);
 }
 
 $(document).keydown(() => {
   if (!started) {
     nextSequnece();
-    $("h1").text("level "+level);
-    started=true;
+    $("h1").text("level " + level);
+    started = true;
   }
-
 });
 
-
 $(".btn").click(function (e) {
-
-//   var userChosenColour = e.target.id;
   var userChosenColour = $(this).attr("id");
-//   $(this).addClass("pressed")
-  animatePress($(this))
+  animatePress($(this));
   playSound("sounds/" + userChosenColour + ".mp3");
   userClickedPattern.push(userChosenColour);
-  
+  console.log(userClickedPattern, gamePattern);
+  if (gamePattern.length >= 1) {
+    for (let i = 0; i < userClickedPattern.length - 1; i++) {
+      console.log(userClickedPattern[i], gamePattern[i]);
+      if (userClickedPattern[i] !== gamePattern[i]) {
+        console.log("wrong");
+        $("h1").text("Game Over, Press Any Key to Restart");
+        started = false;
+        gamePattern = [];
+        userClickedPattern = [];
+        level=0;
+        return;
+      }
+    }
+  }
+  if (started) {
+    setTimeout(() => {
+      nextSequnece();
+    }, 2000);
+  }
+
+  // console.log(gamePattern,userClickedPattern)
 });
