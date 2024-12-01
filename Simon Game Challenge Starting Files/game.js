e9cc54581e18d85f23b405ddfb3f7f024599520c
itsmearun1 <arundevvb1@gmail.com>
2024-12-01 23:50:22 +0530

    tomorrow

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -4,6 +4,14 @@ var buttonColors = ["red", "blue", "green", "yellow"];
 var level = 0;
 var started = false;
 
+//function to start the game
+$(document).keydown(() => {
+  if (!started) {
+    nextSequnece();
+    started = true;
+  }
+});
+
 //Function to play the sound
 function playSound(url) {
   let audio = new Audio(url);
@@ -25,46 +33,38 @@ function nextSequnece() {
   var newId = "#" + randomChosenColour;
   $(newId).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
   playSound("sounds/" + randomChosenColour + ".mp3");
+  // console.log(userClickedPattern, gamePattern);
   level++;
   $("h1").text("level " + level);
 }
 
-
-//function to start the game
-$(document).keydown(() => {
-  if (!started) {
-    nextSequnece();
-    $("h1").text("level " + level);
-    started = true;
-  }
-});
-
-//function to check whether the pattern is correct or not
-function checkPattern() {
-  if (gamePattern.length >= 1) {
-    for (let i = 0; i < userClickedPattern.length - 1; i++) {
-      console.log(userClickedPattern[i], gamePattern[i]);
-      if (userClickedPattern[i] !== gamePattern[i]) {
-        console.log("wrong");
-        $("h1").text("Game Over, Press Any Key to Restart");
-        started = false;
-        gamePattern = [];
-        userClickedPattern = [];
-        level = 0;
-        return;
-      }
-    }
-  }
-  if (started) {
-    setTimeout(() => {
-      nextSequnece();
-    }, 2000);
-  }
-}
 $(".btn").click(function (e) {
   var userChosenColour = $(this).attr("id");
   animatePress($(this));
   playSound("sounds/" + userChosenColour + ".mp3");
   userClickedPattern.push(userChosenColour);
+  checkPattern();
+  // userClickedPattern = [];
+  // setTimeout(() => {
+  //   nextSequnece();
+  // }, 1000);
+
   console.log(userClickedPattern, gamePattern);
 });
+
+// function to check whether the pattern is correct or not
+function checkPattern() {
+  for (let i = 0; i <= userClickedPattern.length - 1; i++) {
+    // console.log(userClickedPattern[i], gamePattern[i]);
+    if (userClickedPattern[i] !== gamePattern[i]) {
+      console.log("wrong");
+      $("h1").text("Game Over, Press Any Key to Restart");
+      started = false;
+      gamePattern = [];
+      userClickedPattern = [];
+      level = 0;
+      return;
+    }
+  }
+  nextSequnece();
+}
