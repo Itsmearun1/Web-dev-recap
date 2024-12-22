6a4b9a78e3f7cfe94ae8b33121d5d87492d79067
Itsmearun1 <arundevvb1@gmail.com>
2024-12-23 01:01:34 +0530

    HTTP basics finished

--- Backend/3.2 HTTP Requests/index.js
+++ Backend/3.2 HTTP Requests/index.js
@@ -1,19 +1,16 @@
 import express from "express";
 const app = express();
-const port = 3000;
+const port=3000;
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello</h1>");
-});
-
-app.get("/about", (req, res) => {
-  res.send("<h1>About Me</h1><p>My name is Angela</p>");
-});
-
-app.get("/contact", (req, res) => {
-  res.send("<h1>Contact Me</h1><p>Phone: +44123456789</p>");
-});
-
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
-});
+app.get("/",(req,res)=>{
+    res.send("<h1>This is my Homepage</h1>")
+})
+app.get("/about",(req,res)=>{
+    res.send("<h1>This is about page</h1>")
+})
+app.get("/contact",(req,res)=>{
+    res.send("<h1>This is my Contact page/h1>")
+})
+app.listen(port,()=>{
+    console.log("listening to 3000")
+})
